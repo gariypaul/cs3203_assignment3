f0b907608d8b248fa1f378604e4e5e660f31eff9
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2023-09-13 17:09:42 -0500

    Implemented product of array func

--- assignment3.py
+++ assignment3.py
@@ -4,3 +4,13 @@ def calculate_sum(numbers):
     for number in numbers:
         sum=sum+number
     return sum
+
+def calc_product(numbers):
+    """Calculates the sum of the array """ 
+    prod=0
+    for number in numbers:
+        if prod ==0:
+            prod += number
+        else:
+            prod *= number
+    return prod
